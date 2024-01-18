63af7c74204d61b33bc23fe58023a8f95a25b310
Vinayak <vinayak.iyer@theflyy.com>
2024-01-18 23:48:39 +0530

    template.

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,9 +1,9 @@
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { supabase } from "../supabase";
+
 
 export default function AuthComponent() {
     return (
-        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }}/>
+        <>
+        Auth Page
+        </>
     )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,4 @@
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { useEffect, useState } from "react";
-import ImageGallery from "../components/gallery";
-import { supabase } from "../supabase";
-import { fetchImages } from "../utils/constants";
-
 export default function Index() {
-  // const [session, setSession] = useState(null);
-  // useEffect(() => {
-  //   supabase.auth.getSession().then(({ data: { session } }) => {
-  //     setSession(session)
-  //   })
-
-  //   const {data: { subscription }} = supabase.auth.onAuthStateChange((_event, session) => {
-  //     setSession(session)
-  //   })
-
-  //   return () => subscription.unsubscribe()
-  // }, [])
-
   const [result, setResult] = useState([]);
   const [loading, setLoading] = useState(false);
   const fetchImage = async () => {
@@ -41,38 +21,7 @@ export default function Index() {
 
   return (
     <>
-      <h4>Moonshot</h4>
-      <div className="query-box">
-        <div className="inputGroup">
-          <input
-            value={query}
-            onChange={(e) => {
-              setQuery(e.target.value);
-            }}
-            type="text"
-          />
-        </div>
-        <button
-          onClick={(e) => {
-            fetchImage();
-          }}
-        >
-          Search
-        </button>
-      </div>
-      <div className="p-2 m-2">
-
-      <ImageGallery
-        result={result}
-        reachedEnd={(e) => {
-          if (e) {
-            // this.props.fetchItems();
-            setOptions({ ...options, page: options.page + 1 });
-            fetchImage();
-          }
-        }}
-      />
-      </div>
+      Index
     </>
   );
 }
